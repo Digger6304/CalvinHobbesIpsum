bc9018e9b3e56946f0f3b78c41f8f10bd727bd94
Digger6304 <90990325+Digger6304@users.noreply.github.com>
2021-09-18 16:07:29 -0700

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,123 @@
+const calvinQuotes = [
+    "So long, Pop! I'm off to check my tiger trap.",
+    "Undoubtedly.",
+    "Mom, can I take up the floorboards in my room and make a secret passageway?",
+    "How quietly do you think we can nail these back in?",
+    "I rigged a tuna fish sandwich yesterday, so I'm sure to have a tiger by now!",
+    "Tigers will do anything for a tuna fish sandwich!",
+    "So Dad, what do I do when I catch a tiger?",
+    "It was Hobbes, Dad! He was jumping on the bed! Honest!", 
+    "Whew! That was a close one!",
+    "Hey! Aren't you going to say good night to Hobbes?!",
+    "Go to sleep, you sissy.",
+    "Outrage! Why should I go to bed? I'm not tired! It's only 7:30! This is tyranny! I'm- zzzzz.",
+    "Get ready! I hear one coming!",
+    "AAIEEE!! A monster in the Hallway!!",
+    "Any monsters under my bed tonight?!",
+    "I'd hate to have to torch one with my flame thrower!",
+    "Mom can I drive on the way back?",
+    "I promise I won't crash.",
+    "Can I work the gas while you steer?",
+    "Tragically, this serene metropolis lies directly beneath the Hoover Dam...",
+    "That smells like barf!",
+    "Ha! I pulled the plug! Down the drain with you! Die, fiend! die, die!!",
+    "Believe it, lady.",
+    "Bad news Dad, your polls are way down.",
+    "Hey Susie Derkins, is that your face, or is a possum stuck in your collar?",
+    "A squid eyeball sandwich.",
+    "Houston, we have a negative on that orbit trajectory.", 
+    "Mom, can I set fire to my bed mattress?",
+    "Can I ride my tricycle on the roof?",
+    "Hello Dad! It is now three in the morning. Do you know where I am?",
+    "Tommy Chesnutt did once, and now nobody does.",
+    "And here comes an inflammable chemical truck! Oh No!!",
+    "Making speed bumps. ",
+    "None dare attack us! We rule all!!",
+    "Yaah! Death to oatmeal!",
+    "You'll never escape, vile glop! Die! Die!",
+    "I'm not eating this green stuff. Yecchh!",
+    "Having transformed myself into a werewolf, I search for human sacrifice!",
+    "I cannot  answer this question, as it is against my religious principles.",
+    "We are a fierce and dirty band of cutthroat pirates!",
+    "A bag of dead bugs I collected from our window sills.",
+    "Fortunately, our motto is &quot;Be prepared.&quot;",
+    "Well, Hobbes, we did it again. We're separated from the troop and hopelessly lost.",
+    "AAAAUGH! Tiger attack!",
+    "You sissy. Mom always takes your side!",
+    "I'm going to be the next Houdini! Tie me to this chair and I'll escape!",
+    "Ha! The great Calvin can escape from anything!",
+    "Ahh! That's tighter! Pull the other way! The other way.",
+    "&quot;Live for revenge,&quot; is gonna be my motto if you don't get me out of this.",
+    "Oh, no, not tortellini! I hate tortellini!! Oh, gross! Yecch Tortellini!!",
+    "Oh, no. That can only mean our real teacher rocketed back to Saturn to report to her superious!",
+    "Always the victor, he lets out a triumphant roar!",
+    "Calvin the criminal is about to face justice! Angry throngs turn out to watch his execution!",
+    "Open your mouth and close your eyes, and you will get a big surprise.",
+    "Uh oh. Hang on, he got away.",
+    "Santa Claus: Kindly old elf, or CIA spook?",
+    "This is a purely hypothetical case, Mr.Smartypants.", 
+
+
+
+
+
+]
+
+
+const hobbesQuotes = [
+   
+    "We're kind of stupid that way.",
+    "No, really, I can't eat another bite!",
+    "Well, you were the one playing the cymbals!!",
+    "You have a flame thrower??",
+    "Yeah. Let me see your bat a minute.",
+    "Nothing to do ?! You could read a book! Or write a letter! Or take a walk!",
+    "That's because she wanted another tiger, not you!",
+    "Who do we smooch then?",
+    "Just so long as we don't get hungry.",
+    "We're lost again.",
+    "Sunny and Warmer today, high in the upper thirties.",
+    "Most children have seen a million murders on television.",
+    "Well, I've always been partial to redheads.",
+    "I think we play saxophone for an all-girl cabaret in New Orleans.",
+    "Should I tell her you're tied up at the moment, ha ha?",
+    "Good thing you're doing this before you tried it under water.",
+    "&quot;Sit tight,&quot; ha ha! I'll get your scout manual. ",
+    "Hey, look, here's a motto! I didn't know you had a motto! Wow, what fun!",
+    "I'll quiz you. What do you do for a second-degree burn?",
+    "Hmph, if I was in your predicament, I'd treat me with a lot more respect. Do you say you're sorry?",
+    "You got yourself into this &quot;Mr. Houdini,&quot; not me.",
+    "Here's how you say &quot;Banana&quot; in morse. Dash dot dot dot, dot dash...",
+    "Your're worried about the salamander incident aren't you?",
+    "Should I tell her you're tied up at the moment, ha ha?",
+    "How cynically enterprising of you.",
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
+
+
+const spiffQuotes = [
+   
+    "The valiant spaceman spiff has been captured!",
+    "The aliens doubtlessly want the secret formula to the atomic napalm neutralizer!",
+    "Moments from the torture chamber, Spiff springs into action.",
+    "I’ll set my fertilizer on deep fat fry.",
+    "We join spaceman spiff on the distant planet Zorg.",
+    "Chew electric death, snarling cur! ",
+    "Suddenly, a searing bolt of deadly frap ray slices across the blackness! Our Hero is unfazed.",
+    "Spiff is going down! Can he make it?? Is this the end?!?",
+    "Spaceman Spiff cooly draws his death ray blaster...",
+    
+
+]
---
+++ scripts.js
@@ -0,0 +1,95 @@
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+
+    $('.ui.checkbox').checkbox();
+    $('.ui.dropdown').dropdown();
+});
+
+
+function generate() {
+
+    var element = document.getElementById('output-ipsum');
+
+    var numparagraphs = document.getElementById("numparagraphs").value;
+
+    if(document.getElementById("calvinradio").checked){
+
+        element.innerHTML = generateCalvinText(numparagraphs);
+        return;
+    }
+
+
+    if(document.getElementById("hobbesradio").checked){
+
+        element.innerHTML = generateHobbesText(numparagraphs);
+        return;
+    }
+
+
+    if(document.getElementById("spiffradio").checked){
+
+        element.innerHTML = generateCalvinText();
+        return;
+    }
+
+
+    if(document.getElementById("lucyradio").checked){
+
+        element.innerHTML = generateCalvinText();
+        return;
+    }
+
+
+    
+}
+
+
+function generateCalvinText(numparagraphs) {
+
+    var quotes = "";
+
+    for(let i = 0; i < numparagraphs ;i++ )    {
+
+        quotes += "<p>";    
+        
+        for(let j = 0; j < 7 ;j++ ){
+
+            var randomindex = Math.floor(Math.random() * calvinQuotes.length);
+
+            quotes += calvinQuotes[randomindex];  
+            quotes += "  ";
+
+        }  
+        
+        quotes += "</p>";
+    }
+    
+    return quotes;
+
+}
+
+function generateHobbesText(numparagraphs) {
+
+    var quotes = "";
+
+    for(let i = 0; i < numparagraphs ;i++ )    {
+
+        quotes += "<p>";    
+        
+        for(let j = 0; j < 7 ;j++ ){
+
+            var randomindex = Math.floor(Math.random() * hobbesQuotes.length);
+
+            quotes += hobbesQuotes[randomindex];  
+            quotes += "  ";
+
+        }  
+        
+        quotes += "</p>";
+    }
+    
+    return quotes;
+
+}
+
